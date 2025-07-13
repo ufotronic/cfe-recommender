8affb4b60b1ad9eaef0d6710a7e6f05d849a2d67
ufotronic <tschueler737@gmail.com>
2025-07-13 14:01:15 +0200

    movies

--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,5 +1,30 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+from cfehome import utils2 as cfehome_utils
+
+
+User = get_user_model()
 
 class Command(BaseCommand):
+
+    def add_arguments(self,parser):
+        parser.add_argument("count", nargs='?', default=10, type=int)
+
+
     def handle(self, *args, **options):
-        print("hello world", options)
+        #print(f"hello there are {User.objects.count()} users")
+
+        count = options.get('count')
+
+        profiles = cfehome_utils.get_fake_profiles(count=count)
+
+        new_users = []
+
+        for profile in profiles:
+            new_users.append(
+                User.objects.create(**profile)
+            )
+        user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
+
+        print(f"New users created {len(user_bulk)}")
