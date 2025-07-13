d16e61a48c834bd86670e66bb39b684fa5d92c0c
ufotronic <tschueler737@gmail.com>
2025-07-13 17:18:08 +0200

    load movies

--- src/cfehome/utils2.py
+++ src/cfehome/utils2.py
@@ -1,5 +1,65 @@
 
+import csv
+
+import datetime
+
+from pprint import pprint
+
+from django.conf import settings
+
 from faker import Faker
+
+
+MOVIE_METADATA_CSV = settings.DATA_DIR / "movies_metadata.csv"
+
+
+def validate_date_str(date_text):
+    try:
+        datetime.datetime.strptime(date_text, "%Y-%m-%d")
+    except:
+        return None
+    return date_text
+
+
+
+
+
+def load_movie_data(limit=10):
+
+    #with open(MOVIE_METADATA_CSV, newline='') as csvfile:
+    # had to add : errors='replace' to avoid UnicodeDecodeError
+
+    with open(MOVIE_METADATA_CSV, newline='', errors='replace') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        dataset = []
+
+        for i, row in enumerate(reader):
+            pprint(row)
+
+            _id = row.get("id")
+
+            try:
+                _id = int(_id)
+            except:
+                _id = None
+                pass
+
+            data = {
+                "id" : _id,
+                "title" : row.get('title'),
+                "overview" : row.get('overview'),
+                "release_date" :    validate_date_str(row.get('release_date'))
+            }
+
+            dataset.append(data)
+
+            if i + 1 > limit:
+                break
+
+        return dataset
+
 
 
 def get_fake_profiles(count=10):
--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+
 from cfehome import utils2 as cfehome_utils
+
+from movies.models import Movie
 
 
 User = get_user_model()
@@ -11,20 +14,42 @@ class Command(BaseCommand):
     def add_arguments(self,parser):
         parser.add_argument("count", nargs='?', default=10, type=int)
 
+        parser.add_argument("--movies", action='store_true', default=False)
+        parser.add_argument("--users", action='store_true', default=False)
+        parser.add_argument("--show-total", action='store_true', default=False)
+
 
     def handle(self, *args, **options):
         #print(f"hello there are {User.objects.count()} users")
 
         count = options.get('count')
 
-        profiles = cfehome_utils.get_fake_profiles(count=count)
+        load_movies = options.get('movies')
+        generate_users = options.get('users')
 
-        new_users = []
+        if load_movies:
+            movie_dataset = cfehome_utils.load_movie_data(limit=count)
 
-        for profile in profiles:
-            new_users.append(
-                User.objects.create(**profile)
-            )
-        user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
+            # unpacking
+            movies_new = [Movie(**x) for x in movie_dataset]
+            movies_bulk = Movie.objects.bulk_create(movies_new, ignore_conflicts=True)
+            print(f"New movies created {len(movies_bulk)}")
 
-        print(f"New users created {len(user_bulk)}")
+            #if show_total:
+            #    print(f"Total movies {Movie.objects.count()}")
+                
+        
+
+        if generate_users:
+
+            profiles = cfehome_utils.get_fake_profiles(count=count)
+
+            new_users = []
+
+            for profile in profiles:
+                new_users.append(
+                    User.objects.create(**profile)
+                )
+            user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
+
+            print(f"New users created {len(user_bulk)}")
