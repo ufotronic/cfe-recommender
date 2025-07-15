754e7cfc05f2703dca026f4299ec473590e955e6
ufotronic <tschueler737@gmail.com>
2025-07-15 11:55:40 +0200

    Fake Ratings

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -7,7 +7,7 @@ from .models import Movie
 class MovieAdmin(admin.ModelAdmin):
 
     list_display = ['__str__', 'calculate_ratings_count']
-    readonly_fields = ['calculate_ratings_count','calculate_ratings_avg']
+    readonly_fields = ['calculate_ratings_count','calculate_ratings_avg','rating_avg_display']
 
 
 admin.site.register(Movie, MovieAdmin)
--- src/movies/models.py
+++ src/movies/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+from django.utils import timezone
+
+import datetime
+
 from django.contrib.contenttypes.fields import GenericRelation
 
 from ratings.models import Rating
 
 # Create your models here.
 
+RATING_CALC_TIME = 1
 
 class Movie(models.Model):
     title = models.CharField(max_length=120, unique=True)
@@ -15,9 +20,38 @@ class Movie(models.Model):
 
     ratings = GenericRelation(Rating) # its a QuerySet
 
+    rating_last_updated = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+
+    rating_count = models.IntegerField(blank=True, null=True)
+    rating_avg = models.DecimalField(decimal_places=2, max_digits=5, blank=True, null=True)
+
+    def rating_avg_display(self):
+
+        now = timezone.now()
+
+        if not self.rating_last_updated:
+            return self.calculate_rating()
+
+        if self.rating_last_updated > (now - datetime.timedelta(minutes=RATING_CALC_TIME) ):
+            return self.rating_avg
+
+        return self.calculate_rating()
+
 
     def calculate_ratings_count(self):
         return self.ratings.all().count()
     
     def calculate_ratings_avg(self):
         return self.ratings.all().avg()
+    
+    def calculate_rating(self, save=True):
+        rating_avg = self.calculate_ratings_avg()
+        rating_count = self.calculate_ratings_count()
+        self.rating_count = rating_count
+        self.rating_avg = rating_avg
+        self.rating_last_updated = timezone.now()
+
+        if save:
+            self.save()
+
+        return self.rating_avg
